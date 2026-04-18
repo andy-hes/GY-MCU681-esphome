e1b51c8e5079069a65c03f50a420b459faf9943e
andy-hes <111559542+andy-hes@users.noreply.github.com>
2026-04-18 15:55:10 +0200

    Update sensor.py
    Corrected

--- components/mcu680/sensor.py
+++ components/mcu680/sensor.py
@@ -1,63 +1,73 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import CONF_ID
+from esphome.components import sensor, uart
+from esphome.const import (
+    CONF_ID,
+    CONF_TEMPERATURE,
+    CONF_HUMIDITY,
+    CONF_PRESSURE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_HECTOPASCAL,
+    UNIT_PERCENT,
+    DEVICE_CLASS_TEMPERATURE,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_ATMOSPHERIC_PRESSURE,
+)
 
 DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["sensor"]
+
+CONF_IAQ = "iaq"
+CONF_IAQ_ACCURACY = "iaq_accuracy"
+CONF_GAS_RESISTANCE = "gas_resistance"
+CONF_ALTITUDE = "altitude"
 
 mcu680_ns = cg.esphome_ns.namespace("mcu680")
 MCU680Component = mcu680_ns.class_("MCU680Component", cg.Component, uart.UARTDevice)
 
-CONF_TEMPERATURE = "temperature"
-CONF_HUMIDITY = "humidity"
-CONF_PRESSURE = "pressure"
-CONF_IAQ = "iaq"
-CONF_IAQ_ACCURACY = "iaq_accuracy"
-CONF_GAS = "gas_resistance"
-CONF_ALTITUDE = "altitude"
-
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(MCU680Component),
-
         cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-            unit_of_measurement="°C",
+            unit_of_measurement=UNIT_CELSIUS,
             accuracy_decimals=1,
-            device_class="temperature",
-            state_class="measurement",
+            device_class=DEVICE_CLASS_TEMPERATURE,
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
-            unit_of_measurement="%",
+            unit_of_measurement=UNIT_PERCENT,
             accuracy_decimals=1,
-            device_class="humidity",
-            state_class="measurement",
+            device_class=DEVICE_CLASS_HUMIDITY,
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_PRESSURE): sensor.sensor_schema(
-            unit_of_measurement="hPa",
+            unit_of_measurement=UNIT_HECTOPASCAL,
             accuracy_decimals=2,
-            device_class="pressure",
-            state_class="measurement",
+            device_class=DEVICE_CLASS_ATMOSPHERIC_PRESSURE,
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_IAQ): sensor.sensor_schema(
             accuracy_decimals=0,
-            state_class="measurement",
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_IAQ_ACCURACY): sensor.sensor_schema(
             accuracy_decimals=0,
-            state_class="measurement",
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category="diagnostic",
         ),
-        cv.Optional(CONF_GAS): sensor.sensor_schema(
+        cv.Optional(CONF_GAS_RESISTANCE): sensor.sensor_schema(
             unit_of_measurement="kΩ",
             accuracy_decimals=1,
-            state_class="measurement",
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
         cv.Optional(CONF_ALTITUDE): sensor.sensor_schema(
             unit_of_measurement="m",
             accuracy_decimals=0,
-            state_class="measurement",
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
     }
-).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 
 async def to_code(config):
@@ -65,30 +75,30 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if CONF_TEMPERATURE in config:
-        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
+    if conf := config.get(CONF_TEMPERATURE):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_temperature_sensor(sens))
 
-    if CONF_HUMIDITY in config:
-        sens = await sensor.new_sensor(config[CONF_HUMIDITY])
+    if conf := config.get(CONF_HUMIDITY):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_humidity_sensor(sens))
 
-    if CONF_PRESSURE in config:
-        sens = await sensor.new_sensor(config[CONF_PRESSURE])
+    if conf := config.get(CONF_PRESSURE):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_pressure_sensor(sens))
 
-    if CONF_IAQ in config:
-        sens = await sensor.new_sensor(config[CONF_IAQ])
+    if conf := config.get(CONF_IAQ):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_iaq_sensor(sens))
 
-    if CONF_IAQ_ACCURACY in config:
-        sens = await sensor.new_sensor(config[CONF_IAQ_ACCURACY])
+    if conf := config.get(CONF_IAQ_ACCURACY):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_iaq_accuracy_sensor(sens))
 
-    if CONF_GAS in config:
-        sens = await sensor.new_sensor(config[CONF_GAS])
-        cg.add(var.set_gas_sensor(sens))
+    if conf := config.get(CONF_GAS_RESISTANCE):
+        sens = await sensor.new_sensor(conf)
+        cg.add(var.set_gas_resistance_sensor(sens))
 
-    if CONF_ALTITUDE in config:
-        sens = await sensor.new_sensor(config[CONF_ALTITUDE])
+    if conf := config.get(CONF_ALTITUDE):
+        sens = await sensor.new_sensor(conf)
         cg.add(var.set_altitude_sensor(sens))
